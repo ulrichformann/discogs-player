d7703ebba669f18aa33d51a6a7ca037f2022714a
Ulrich Formann <ulrich.f@live.at>
2020-04-12 16:24:45 +0200

    add file selection mechanism

--- app/renderer/components/FileSelection.tsx
+++ app/renderer/components/FileSelection.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { useState, useRef, useEffect } from 'react'
 
 import { FileIcon } from './Section'
 
@@ -43,7 +44,7 @@ const Files = styled.div`
   }
 `
 
-const File = styled.div`
+const File = styled.div<{ dragOver?: boolean }>`
   margin: 8px 0 0;
   height: 32px;
   background: #2C2C2E;
@@ -57,7 +58,6 @@ const File = styled.div`
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: nowrap;
-  cursor: grab;
 
   :first-child {
     margin-top: 12px;
@@ -66,6 +66,18 @@ const File = styled.div`
   :last-child {
     margin-bottom: 16px;
   }
+
+  transition: all 333ms ease;
+
+  ${p => p.draggable && `cursor: grab;`}
+  
+  ${p => p.onClick && `cursor: w-resize;`}
+
+  ${p => p.dragOver && `
+    color: #1C1C1E;
+    background: #1C1C1E;
+    border: 0;
+  `}
 `
 
 const DropFile = styled(File)`
@@ -73,10 +85,10 @@ const DropFile = styled(File)`
   border: 1px dashed #48484A;
 `
 
-const Cover = styled.div`
+const Cover = styled.div<{url?: string}>`
   height: 100%;
   opacity: 0.8;
-  background-image: url('https://img.discogs.com/vqmbKQxHQFLZbxlmvS68nEQ-8bM=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2-1193657839.jpeg.jpg');
+  background-image: url('${p => p.url}');
   background-size: cover;
   background-position: center center;
 `
@@ -84,17 +96,34 @@ const Cover = styled.div`
 const CoverWrapper = styled.div`
   height: 48px;
   background: white;
+`
 
-  button {
+const CancelButton = styled.button`
+  position: absolute;
+  background: 0;
+  top: 12px;
+  right: 16px;
+  width: 24px;
+  height: 24px;
+  border: 0;
+  padding: 0;
+  cursor: pointer;
+
+  :before, :after {
+    content: '';
     position: absolute;
-    top: 12px;
-    right: 16px;
-    width: 24px;
-    height: 24px;
-    padding: 2px 0 0;
-    background: 0;
-    border: 0;
-    cursor: pointer;
+    left: 50%;
+    top: 50%;
+    width: 18px;
+    height: 0px;
+    transform: translate(-50%, -50%) rotate(45deg);
+    border: 0.5px solid black;
+    border-radius: 0.5px;
+    transition: all 333ms ease;
+  }
+
+  :after {
+    transform: translate(-50%, -50%) rotate(-45deg);
   }
 `
 
@@ -108,57 +137,107 @@ interface Props {
     ext: string,
     mime: string
   }[]
+  data: any
 }
 
 export default (props: Props) => {
-  const title = '2001 asd asjdhas kdjasdasdpakjashd aksjhdk'
+  const [ draggedFile, setDraggedFile ] = useState(null)
+  const [ dropFileDragOver, setDropFileDragOver ] = useState(null)
+  const [ files, setFiles ] = useState([])
+
+  useEffect(() => {
+    setFiles(props.data.tracklist.map(e => ({ 
+      ...e,
+      file: null,
+      fileIndex: null,
+    })))
+
+  }, [ props.data ])
+
+  const handleDrop = () => {
+    const newFiles = files
+
+    newFiles[dropFileDragOver].file = props.files[draggedFile]
+    newFiles[dropFileDragOver].fileIndex = draggedFile
+
+    setFiles(newFiles)
+    setDropFileDragOver(null)
+  }
+
+  const handleDragOver = (e, i) => {
+    e.preventDefault()
+    setDropFileDragOver(i)
+  }
+
+  const handleClick = (e, i) => {
+    e.preventDefault()
+    
+    const newFiles = [ ...files ]
+    
+    newFiles[i].file = null
+    newFiles[i].fileIndex = null
+    
+    console.log(newFiles)
+    
+    setFiles(newFiles)
+  }
 
   return (
     <>
       <Wrapper>
         <Title>
-          <FileIcon/>
-          <div>{title}</div>
+          <FileIcon />
+          <div>{props.folder}</div>
         </Title>
         <Files>
-          <File draggable >Aasdaksdjlasjdlajsldjlasjdlasjdlasjdlasjdljasldjalksjd</File>
-          <File draggable >asd</File>
-          <File draggable >Aasdaksdjlasjdlajsldjlasjdlasjdlasjdlasjdljasldjalksjd</File>
-          <File draggable >asd</File>
-          <File draggable >asd</File>
-          <File draggable >asd</File>
-          <File draggable >asd</File>
-          <File draggable >asd</File>
-          <File draggable >asd</File>
-          <File draggable >asd</File>
+          {props.files.map((e, i) => (
+            (files.map(e => e.fileIndex).indexOf(i) === -1) &&
+            <File 
+              key={i}
+              draggable 
+              onDragStart={() => setDraggedFile(i)}
+              onDragEnd={() => setDraggedFile(null)} 
+              onDragOver={e => e.preventDefault()}
+            >
+              {e .name}
+            </File>
+          ))}
         </Files>
       </Wrapper>
       {props.searched && 
         <DropWrapper>
           <CoverWrapper>
-            <Cover>
+            <Cover url={props.data.images.find(e => e.type === 'primary').uri}>
             </Cover>
-            <button onClick={props.onCancel}>
-              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13 1L1 13" stroke="#E5E5EA" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M1 1L13 13" stroke="#E5E5EA" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </button>
+            <CancelButton onClick={props.onCancel} />
           </CoverWrapper>
-
           <Files>
-            <DropFile>asdasdhalsjhdlahsdjkhs</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
-            <DropFile>asd</DropFile>
+            {files.map((e, i) => (
+              !e.file ? 
+                <DropFile 
+                  key={i} 
+                  onDragOver={e => handleDragOver(e, i)}
+                  onDragLeave={() => setDropFileDragOver(null)}
+                  dragOver={dropFileDragOver === i}
+                  onDrop={handleDrop} 
+                >
+                  {!!e.position && `${e.position} - `}
+                  {e.title}
+                </DropFile>
+              :
+                <File 
+                  key={i}
+                  onDragOver={e => handleDragOver(e, i)}
+                  onDragLeave={() => setDropFileDragOver(null)}
+                  dragOver={dropFileDragOver === i}
+                  onDrop={handleDrop}
+                  onClick={(e) => handleClick(e, i)}
+                >
+                  {e.file.name}
+                </File>
+            ))}
           </Files>
-          </DropWrapper>}
+        </DropWrapper>}
     </>
   )
 }
